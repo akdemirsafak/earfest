777e5b530bb98931e4120cd9c991d9c4f52e91e8
akdemirsafak <akdemirsafak@gmail.com>
2025-01-17 12:49:02 +0300

    Entities Added

---
+++ earPass/earPass.Domain/Common/EventTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace earPass.Domain.Enums;
+
+public enum EventTypeEnum
+{
+    Music = 1,
+    Theater = 2,
+    Movie = 3,
+    Sport = 4,
+    Conference = 5,
+    Festival = 6,
+    Other = 7
+}
---
+++ earPass/earPass.Domain/Entities/UserTicket.cs
@@ -0,0 +1,19 @@
+﻿
+using earPass.Domain.Common;
+
+namespace earPass.Domain.Entities;
+
+public sealed class UserTicket : AbstractEntity 
+{
+    public string EventName { get; set; }
+    public string TicketName { get; set; }
+    public string TicketId { get; set; }
+    public decimal Price { get; set; }
+    public string UserId { get; set; }
+    public string UserFullName { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public bool IsUsed { get; set; }
+    public bool IsExpired { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public string QRCode { get; set; }
+}
