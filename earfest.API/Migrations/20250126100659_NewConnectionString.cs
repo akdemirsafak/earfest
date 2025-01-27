26a7fad524c22398ada0d0bfc5bc495708d60d91
Şafak Akdemir <akdemirsafak34@hotmail.com>
2025-01-27 13:00:07 +0300

    Update DB schema, connection string, and add token response
    - Added migration `NewConnectionString` to update the database schema, including the creation of various entities and relationships, and the removal of `CreatedBy` and `UpdatedBy` columns from the `AspNetUsers` table.
    - Updated `EarfestDbContextModelSnapshot.cs` to reflect the schema changes.
    - Modified connection string name in `Program.cs` from `"EarfestContext"` to `"EarfestDbContext"`.
    - Changed default status code in `AppResult.cs` `Fail` method from `0` to `400`.
    - Added `AppTokenResponse` class to represent the structure of a token response.

---
+++ earfest.API/Migrations/20250126100659_NewConnectionString.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace earfest.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewConnectionString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
