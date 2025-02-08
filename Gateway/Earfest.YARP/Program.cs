d224838f2d07b07b62ab0b4dfdf9d52fab8611c2
Şafak Akdemir <akdemirsafak34@hotmail.com>
2025-02-09 02:29:47 +0300

    Add Earfest.YARP project with .NET 9.0 support
    Updated project to use .NET 9.0 SDK and added package references for OpenAPI and YARP Reverse Proxy. Configured HTTP endpoints, logging, and reverse proxy settings. Added launch settings and development configuration. Updated solution file to include the new Earfest.YARP project.

---
+++ Gateway/Earfest.YARP/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapReverseProxy();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
